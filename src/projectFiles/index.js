75e94e9920279bb3689259211ee98345739a85c8
YutoGamesYT <85291415+YutoGamesYT@users.noreply.github.com>
2022-06-12 15:08:41 +0900

    2022/06/12/15:08

--- src/projectFiles/index.js
+++ src/projectFiles/index.js
@@ -1,4 +1,8 @@
-const { Client, Collection, Intents } = require("discord.js")
+/* =============Import modules============= */
+const { Client, Intents, Collection } = require("discord.js")
+/* ======================================== */
+
+/* ==========Creating Definitions========== */
 const client = new Client({
     // setting intents
     intents: [
@@ -7,22 +11,20 @@ const client = new Client({
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_VOICE_STATES,
     ],
-    ws: {
-        // set smartphone Activitiy
-        properties: {
-            $browser: "Discord iOS",
-        },
-    },
 });
-module.exports = client;
+/* ======================================== */
 
+module.exports = client; // export client
+
+/* ================Settings================ */
 client.commands = new Collection();
 client.slashCommands = new Collection();
 client.config = require("./src/config/config.json");
-
 require("./src/handler")(client); // load handler
-
-client.login(client.config.token).then(() => {
+/* ======================================== */
+client.login(client.config.canary_token).then(() => {
+    // clear console messages
     console.clear()
+    // when the bot logs in, send a success message to the console.
     console.log('正常に起動しました')
 })
